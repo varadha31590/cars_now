de29445320e0b45dbefc121f174a38856c2bed8d
varadharajan <varadha31590@gmail.com>
2024-02-12 17:57:24 +0530

    data added for cars list and logic changed for search
    data added for cars list and logic changed for search

--- src/helper.ts
+++ src/helper.ts
@@ -33,27 +33,32 @@ export class Helper {
 
     public static getFilteredData(allCars: any, filterCondition: any): CarDetail[] {
 
-       // filterCondition = { ...filterCondition, model: filterCondition.model.toString() }
+        // filterCondition = { ...filterCondition, model: filterCondition.model.toString() }
 
         let result = allCars
-            .filter((item: any) => filterCondition.location === item.location)
+            .filter((item: any) => {
+                if (filterCondition.location)
+                    return filterCondition.location === item.location
+                else
+                    return true;
+            })
             .filter((item: any) => {
                 if (filterCondition.model.length > 0)
                     return filterCondition.model.includes(item.model);
                 else
                     return true;
             })
-            .filter((item:any) => {
-                if(filterCondition.numOfOwners)
+            .filter((item: any) => {
+                if (filterCondition.numOfOwners)
                     return filterCondition.numOfOwners === item.numOfOwners;
                 else
                     return true;
             })
-            .filter((item:any) =>{
-                if(filterCondition.transmission)
-                return filterCondition.transmission === item.transmission;
-            else
-                return true;
+            .filter((item: any) => {
+                if (filterCondition.transmission)
+                    return filterCondition.transmission === item.transmission;
+                else
+                    return true;
             });
 
         return result;
